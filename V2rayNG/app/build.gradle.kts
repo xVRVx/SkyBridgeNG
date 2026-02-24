0661b19e645692c28be9128f379f0f26bd25cca3
xVRVx <vrvxvrv@gmail.com>
2026-02-24 20:47:15 +0300

    Маршрутизация 2

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.skybridge.ng"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.2"
+        versionCode = 3
+        versionName = "1.0.3"
         multiDexEnabled = true
 
         val abiFilterList = (properties["ABI_FILTERS"] as? String)?.split(';')
--- V2rayNG/app/src/main/java/com/v2ray/ang/ui/MainActivity.kt
+++ V2rayNG/app/src/main/java/com/v2ray/ang/ui/MainActivity.kt
@@ -104,15 +104,15 @@ class MainActivity : HelperBaseActivity(), NavigationView.OnNavigationItemSelect
         }
 
         // Делаем ссылки в тексте кликабельными
-        binding.tvEmptyProfileMsg.movementMethod = android.text.method.LinkMovementMethod.getInstance()
+        // binding.tvEmptyProfileMsg.movementMethod = android.text.method.LinkMovementMethod.getInstance()
 
         // Подписываемся на сигнал обновления списка серверов
-        mainViewModel.updateListAction.observe(this) {
-            binding.tvEmptyProfileMsg.isVisible = mainViewModel.serversCache.isEmpty()
-        }
+        // mainViewModel.updateListAction.observe(this) {
+        //    binding.tvEmptyProfileMsg.isVisible = mainViewModel.serversCache.isEmpty()
+        // }
 
         // Делаем контрольную проверку 1 раз при запуске экрана
-        binding.tvEmptyProfileMsg.isVisible = mainViewModel.serversCache.isEmpty()
+        // binding.tvEmptyProfileMsg.isVisible = mainViewModel.serversCache.isEmpty()
 
     }
 
