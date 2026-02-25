dcc704932877bd84f5ed747a3449e8d8eaa48891
xVRVx <vrvxvrv@gmail.com>
2026-02-25 10:26:16 +0300

    Новая иконка

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.skybridge.ng"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.0.3"
+        versionCode = 4
+        versionName = "1.0.4"
         multiDexEnabled = true
 
         val abiFilterList = (properties["ABI_FILTERS"] as? String)?.split(';')
